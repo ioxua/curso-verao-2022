d58af7116227d1ab9c4c727f333544ef9bdb43c2
Yehoshua Oliveira <ioxua@newgo.com.br>
2022-02-09 21:15:03 -0300

    Starting the eigth lession

---
+++ aula8/example.java
@@ -0,0 +1,18 @@
+package aula8;
+
+class Super {
+    protected int a = 12;
+}
+
+class Sub extends Super {
+    int getA() {
+        return this.a;
+    }
+}
+
+public class example {
+    public static void main(String[] args) {
+        System.out.println("aspdsapd");
+    }
+}
+
